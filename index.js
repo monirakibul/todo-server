85d9b412e1fb0b2c7fef0f0890697bc2da00a9ac
monirakibul <monirakibul@gmail.com>
2022-05-19 00:50:29 +0600

    comment added

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
-
+// middleware 
 app.use(cors())
 app.use(express.json())
 
+// mongo db connetion 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdgdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -20,6 +21,8 @@ async function run() {
         await client.connect()
         const taskCollection = client.db('toDoList').collection('list')
 
+
+        // get task list filter by email 
         app.get('/task', async (req, res) => {
             const email = req.query.email;
             const query = { email: email }
@@ -28,12 +31,14 @@ async function run() {
             res.send(tasks)
         });
 
+        // add new tast 
         app.post('/add', async (req, res) => {
             const booking = req.body;
             const result = await taskCollection.insertOne(booking);
             return res.send({ success: true, result });
         });
 
+        // delete task by id 
         app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -41,6 +46,7 @@ async function run() {
             res.send(result);
         });
 
+        //task mark as completed 
         app.put('/completed/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
